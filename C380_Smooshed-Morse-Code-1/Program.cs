61c4a764032d5d200dbec12b125849244f8d1b97
Wemmer, Daniel <daniel.wemmer@kumavision.com>
2021-01-26 10:26:28 +0100

    Added Project

---
+++ C380_Smooshed-Morse-Code-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C380_Smooshed_Morse_Code_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
