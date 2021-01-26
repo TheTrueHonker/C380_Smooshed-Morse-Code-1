8852af8aa477ec7bd051d53b707ab259889195b6
Wemmer, Daniel <daniel.wemmer@kumavision.com>
2021-01-26 10:47:09 +0100

    Added Morsecode-Class

---
+++ C380_Smooshed-Morse-Code-1/Morsecodes/Morsecode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C380_Smooshed_Morse_Code_1.Translators;
+
+namespace C380_Smooshed_Morse_Code_1
+{
+    public class SmooshedMorsecode
+    {
+        public long Dashes { get; set; }
+        public long Dots { get; set; }
+        public string Text { get; }
+        public string Morse { get; }
+
+        public SmooshedMorsecode(string text)
+        {
+            Text = text;
+            Morse = SmooshedMorseTranslator.smorse(text);
+            RecountDashesDots();
+
+        }
+
+        protected void RecountDashesDots()
+        {
+            long dashesCount = 0;
+            long dotsCount = 0;
+
+            for(int i = 0; i < Morse.Length; i++)
+            {
+                if (Morse[i] == '-')
+                    dashesCount++;
+                if (Morse[i] == '.')
+                    dotsCount++;
+            }
+
+            Dashes = dashesCount;
+            Dots = dotsCount;
+        }
+    }
+}
